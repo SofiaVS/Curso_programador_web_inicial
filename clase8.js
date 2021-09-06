40c7b0495d1ead2471674c3f2a434a1c81599b63
SofiaVS <sofiastehr@gmail.com>
2021-09-05 21:27:57 -0300

    Página final

---
+++ clase8.js
@@ -0,0 +1,11 @@
+//Funciones para la botonera 
+function mostrar(bloque) {
+	var caja = document.getElementById(bloque)
+	caja.style.display = 'block'
+}
+
+function ocultar(bloque) {
+	var caja = document.getElementById(bloque)
+	caja.style.display = 'none'
+}
+
---
+++ funciones.js
@@ -0,0 +1,7 @@
+var nombre = prompt('¿Cuál es tu nombre?')
+var edad = prompt('¿Qué edad tenés?')
+var piel = prompt('¿Qué tipo de piel tenés? Rellená con "Grasa", "Intermedia" o "Seca"')
+var rutina = confirm('¿Tenés una rutina para cuidar tu piel?')
+var cliente_r = confirm('¿Conocías nuestra estética?')
+
+
